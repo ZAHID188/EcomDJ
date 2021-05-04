8317fb15c21cf4bdcf772012b36b255d97beb1f0
ZAHID188 <zahidpranta5@gmail.com>
2021-05-04 19:51:18 +0600

    Cart functionality make
    Before Cart was only on the nav bar now we have added the functions to perform the cart.

--- ecom_vue/src/views/Search.vue
+++ ecom_vue/src/views/Search.vue
@@ -5,12 +5,13 @@
         <div class="column is 12">
             <h1 class="title"> Search</h1>
             <h2 class="is-size-5 has-text-grey">Search term:"{{query}}"</h2>
-        </div>
+        
 
         <ProductBox
              v-for="product in products"
              v-bind:key="product.id"
              v-bind:product="product"/>
+             </div>
 
     </div>
 
