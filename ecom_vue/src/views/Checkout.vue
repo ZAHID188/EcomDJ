5dd11c4ed1a6f0ccaf1d74829f1ca4583f7d4f35
ZAHID188 <zahidpranta5@gmail.com>
2021-05-22 01:17:43 +0600

    finalizing checkout
    shipping details

--- ecom_vue/src/views/Checkout.vue
+++ ecom_vue/src/views/Checkout.vue
@@ -19,9 +19,10 @@
                       </thead>
                       <tbody>
                           <tr v-for="item in cart.items"
-                          v-bind:key="item.product.id">
+                          v-bind:key="item.product.id"
+                          >
                           <td>{{item.product.name}}</td>
-                          <td>{{item.product.price}}Tk</td>  
+                          <td>{{item.product.price}}</td>  
                           <td> {{item.quantity}}</td>
                           <td> {{ getItemTotal(item).toFixed(2)}}</td>
                               
@@ -39,6 +40,74 @@
                   </table>
 
                   <p v-else>You Don't have any products in your cart..</p> 
+
+              <div class="column is-12 box">
+                  <h2 class="subtitle">Shipping details</h2>
+                  <p class="has-text-grey mb-4">* All fields required</p>
+                  <div class="columns is-multiline">
+                      <div class="column is-6">
+                          <div class="field">
+                              <label>First Name*</label>
+                              <div class="control">
+                                  <input type="text" class="input" v-model="first_name">
+                              </div>
+                          </div>
+
+                          <div class="field">
+                              <label>Last Name*</label>
+                              <div class="control">
+                                  <input type="text" class="input" v-model="last_name">
+                              </div>
+                          </div>
+
+                          <div class="field">
+                              <label>Email*</label>
+                              <div class="control">
+                                  <input type="email" class="input" v-model="email">
+                              </div>
+                          </div>
+                          <div class="field">
+                              <label>Phone*</label>
+                              <div class="control">
+                                  <input type="text" class="input" v-model="phone">
+                              </div>
+                          </div>
+                      </div>
+                        <div class="column is-6">
+                          <div class="field">
+                              <label>Address*</label>
+                              <div class="control">
+                                  <input type="text" class="input" v-model="address">
+                              </div>
+                          </div>
+
+                          <div class="field">
+                              <label>Zip Code*</label>
+                              <div class="control">
+                                  <input type="text" class="input" v-model="zipcode">
+                              </div>
+                          </div>
+                          <div class="field">
+                              <label>Place*</label>
+                              <div class="control">
+                                  <input type="text" class="input" v-model="place">
+                              </div>
+                           </div>
+                        </div>
+                  </div>
+                  <div class="notification is-danger mt-4" v-if="errors.length">
+                            <p v-for="error in errors" v-bind:key="error">{{error}}</p>
+                        </div>
+                       <hr>
+                       <div id="card-element" class="mb-5"></div>
+                       <template v-if="cartTotalLength">
+                           <hr>
+                           <button class="button is-dark" @click="submitForm">Pay with Stripe</button>
+                       </template> 
+                        
+                      
+
+              </div>
             </div>
 
 
@@ -75,19 +144,51 @@ export default {
         getItemTotal(item){
             return item.quantity * item.product.price
         },
+        submitForm(){
+            this.errors=[]
+            if(this.first_name==='')
+            {
+                this.errors.push('The First name field is missing')
+            }
+            if(this.last_name==='')
+            {
+                this.errors.push('The  last_name field is missing')
+            }
+            if(this.phone==='')
+            {
+                this.errors.push('The phone field is missing')
+            }
+            if(this.email==='')
+            {
+                this.errors.push('The email field is missing')
+            }
+            if(this.address==='')
+            {
+                this.errors.push('The address field is missing')
+            }
+            if(this.zipcode==='')
+            {
+                this.errors.push('The zipcode field is missing')
+            }
+            if(this.place==='')
+            {
+                this.errors.push('The place field is missing')
+            }
+        },
   
     },
    computed:{
+       cartTotalPrice(){
+            return this.cart.items.reduce((acc,curVal)=>{
+                return acc +=    curVal.product.price * curVal.quantity
+            },0)
+        },
         cartTotalLength(){
             return this.cart.items.reduce((acc,curVal)=>{
                 return acc += curVal.quantity
             },0)
         },
-        cartTotalPrice(){
-            return this.cart.items.reduce((acc,curVal)=>{
-                return acc +=    curVal.product.price * curVal.quantity
-            },0)
-        },
+        
         
     }
 
