9c1d5cdae2508acff3de482f01bdf34bc3daffaf
ZAHID188 <zahidpranta5@gmail.com>
2021-05-30 22:22:02 -0700

    fixing the error for add to cart button

--- ecom_vue/src/store/index.js
+++ ecom_vue/src/store/index.js
@@ -21,12 +21,12 @@ export default createStore({
       }
     },
     addToCart(state,item){
-      const exists= state.cart.items.filter(i=> i.product.id ===item.product.id)
+      const exists= state.cart.items.filter(i => i.product.id ===item.product.id)
       if(exists.length){
         exists[0].quantity = parseInt(exists[0].quantity)+ parseInt(item.quantity)
       }
       else{
-        state.cart.items.push(items)
+        state.cart.items.push(item)
     }
     localStorage.setItem('cart',JSON.stringify(state.cart))
   }
